262d25ace6e52f8340501a752bebd1f0f19568b3
zadrok <lachlan.e.haggart@gmail.com>
2019-03-06 17:58:39 +1100

    a

--- stream.py
+++ stream.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
 from client import Client
-from setver import Server
+from server import Server
 import sys
 
 if __name__ == '__main__':
   if sys.argv[1] == 'client':
-    Client(ip=sys.argv[2]).run()
+    if len( sys.argv ) == 3:
+      Client(ip=sys.argv[2]).run()
+    else:
+      Client().run()
   elif sys.argv[1] == 'server':
     Server().run()
